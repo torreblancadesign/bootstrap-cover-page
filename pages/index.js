8019396c99d737d37bffa3961d7a46c890b0464b
Enrique Torreblanca <enriquetchat@gmail.com>
2023-04-20 16:39:43 -0700

    File updated, no comment added

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,12 @@ import Component from '../components/component';
 
 const Index = () => {
   return (
-    <div className='cover-container d-flex w-100 h-100 p-3 mx-auto flex-column'>
+    <div className='cover-container d-flex w-1000 h-1000 p-3 mx-auto flex-column'>
       <Component />
     </div>
   );
 }
 
 export default Index;
+
+ 
