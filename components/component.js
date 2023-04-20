14b8a64aadcd43b22663dbc78d29df120bd2277f
Enrique Torreblanca <enriquetchat@gmail.com>
2023-04-20 15:57:24 -0700

    Add components/component.js

---
+++ components/component.js
@@ -0,0 +1,24 @@
+
+
+import React from 'react';
+import styles from '../styles/style.module.css';
+const customCover = () => {
+  return (
+    <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <main role="main" className="inner cover">
+        <h1 className="cover-heading">Cover your page.</h1>
+        <p className="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+        <p className="lead">
+          <a href="#" className="btn btn-lg btn-secondary">Learn more</a>
+        </p>
+      </main>
+      <footer className="mastfoot mt-auto">
+        <div className="inner">
+          <p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default customCover;
