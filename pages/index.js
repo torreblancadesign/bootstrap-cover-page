8f334673b6617bd190a0212ae99d640d45191a35
Enrique Torreblanca <enriquetchat@gmail.com>
2023-04-20 15:57:24 -0700

    Add pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+
+
+
+import React from 'react';
+import Component from '../components/component';
+import styles from '../styles/style.module.css';
+const Index = () => {
+  return (
+    <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <Component />
+    </div>
+  );
+}
+
+export default Index;
